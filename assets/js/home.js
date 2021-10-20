8063ba7764aa1b219851548b0f8ff56ad12af8a8
HenryUzo <huzo93.hu@gmail.com>
2021-10-20 08:10:44 +0100

    20-10-20

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,160 +1,147 @@
-
 // Modal video popup
 
-$(document).ready(function(){
-
-
-	/* Toggle Video Modal
+$(document).ready(function () {
+  /* Toggle Video Modal
   -----------------------------------------*/
-	function toggle_video_modal() {
-	    
-	    // Click on video thumbnail or link
-	    $(".js-trigger-video-modal").on("click", function(e){
-          
-          // prevent default behavior for a-tags, button tags, etc. 
-	        e.preventDefault();
-        
-          // Grab the video ID from the element clicked
-          var id = $(this).attr('data-youtube-id');
+  function toggle_video_modal() {
+    // Click on video thumbnail or link
+    $(".js-trigger-video-modal").on("click", function (e) {
+      // prevent default behavior for a-tags, button tags, etc.
+      e.preventDefault();
 
-          // Autoplay when the modal appears
-          // Note: this is intetnionally disabled on most mobile devices
-          // If critical on mobile, then some alternate method is needed
-          var autoplay = '?autoplay=1';
+      // Grab the video ID from the element clicked
+      var id = $(this).attr("data-youtube-id");
 
-          // Don't show the 'Related Videos' view when the video ends
-          var related_no = '&rel=0';
+      // Autoplay when the modal appears
+      // Note: this is intetnionally disabled on most mobile devices
+      // If critical on mobile, then some alternate method is needed
+      var autoplay = "?autoplay=1";
 
-          // String the ID and param variables together
-          var src = '//www.youtube.com/embed/'+id+autoplay+related_no;
-          
-          // Pass the YouTube video ID into the iframe template...
-          // Set the source on the iframe to match the video ID
-          $("#youtube").attr('src', src);
-          
-          // Add class to the body to visually reveal the modal
-          $("body").addClass("show-video-modal noscroll");
-	    
-      });
+      // Don't show the 'Related Videos' view when the video ends
+      var related_no = "&rel=0";
 
-	    // Close and Reset the Video Modal
-      function close_video_modal() {
-        
-        event.preventDefault();
+      // String the ID and param variables together
+      var src = "//www.youtube.com/embed/" + id + autoplay + related_no;
 
-        // re-hide the video modal
-        $("body").removeClass("show-video-modal noscroll");
+      // Pass the YouTube video ID into the iframe template...
+      // Set the source on the iframe to match the video ID
+      $("#youtube").attr("src", src);
 
-        // reset the source attribute for the iframe template, kills the video
-        $("#youtube").attr('src', '');
-        
+      // Add class to the body to visually reveal the modal
+      $("body").addClass("show-video-modal noscroll");
+    });
+
+    // Close and Reset the Video Modal
+    function close_video_modal() {
+      event.preventDefault();
+
+      // re-hide the video modal
+      $("body").removeClass("show-video-modal noscroll");
+
+      // reset the source attribute for the iframe template, kills the video
+      $("#youtube").attr("src", "");
+    }
+    // if the 'close' button/element, or the overlay are clicked
+    $("body").on(
+      "click",
+      ".close-video-modal, .video-modal .overlay",
+      function (event) {
+        // call the close and reset function
+        close_video_modal();
       }
-      // if the 'close' button/element, or the overlay are clicked 
-	    $('body').on('click', '.close-video-modal, .video-modal .overlay', function(event) {
-	        
-          // call the close and reset function
-          close_video_modal();
-	        
-      });
-      // if the ESC key is tapped
-      $('body').keyup(function(e) {
-          // ESC key maps to keycode `27`
-          if (e.keyCode == 27) { 
-            
-            // call the close and reset function
-            close_video_modal();
-            
-          }
-      });
-	}
-	toggle_video_modal();
-
-
-
+    );
+    // if the ESC key is tapped
+    $("body").keyup(function (e) {
+      // ESC key maps to keycode `27`
+      if (e.keyCode == 27) {
+        // call the close and reset function
+        close_video_modal();
+      }
+    });
+  }
+  toggle_video_modal();
 });
 
+var owl = $("#owl-carousel-1");
+owl.owlCarousel({
+  autoplay: true,
+  lazyLoad: true,
+  rewind: true,
+  responsiveClass: true,
+  autoHeight: true,
+  autoplayTimeout: 7000,
+  smartSpeed: 800,
+  margin: 10,
+  loop: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1300: {
+      items: 3,
+    },
 
+    1900: {
+      items: 4,
+    },
+  },
+});
 
-var owl = $('#owl-carousel-1');
-      owl.owlCarousel({
-        autoplay: true,
-        lazyLoad: true,
-        rewind: true,
-        responsiveClass: true,
-        autoHeight: true,
-        autoplayTimeout: 7000,
-        smartSpeed: 800,
-        margin: 10,
-        loop: true,
-        responsive: {
-          0: {
-            items: 1
-          },
-          600: {
-            items: 2
-          },
-          1300: {
-            items: 3
-          },
-         
-          1900:{
-            items:4
-        }
-        }
-      })
+var owl = $("#owl-carousel-2");
+owl.owlCarousel({
+  autoplay: true,
+  lazyLoad: true,
+  rewind: true,
+  responsiveClass: true,
+  autoHeight: true,
+  autoplayTimeout: 8000,
+  smartSpeed: 800,
+  margin: 10,
+  loop: true,
+  responsive: {
+    0: {
+      items: 2,
+    },
+    600: {
+      items: 3,
+    },
+    1300: {
+      items: 5,
+    },
 
-      var owl = $('#owl-carousel-2');
-      owl.owlCarousel({
-        autoplay: true,
-        lazyLoad: true,
-        rewind: true,
-        responsiveClass: true,
-        autoHeight: true,
-        autoplayTimeout: 8000,
-        smartSpeed: 800,
-        margin: 10,
-        loop: true,
-        responsive: {
-          0: {
-            items: 2
-          },
-          600: {
-            items: 3
-          },
-          1300: {
-            items: 5
-          },
-         
-          1600:{
-            items:6
-        }
-        }
-      })
+    1600: {
+      items: 6,
+    },
+  },
+});
 
-      var owl = $('#owl-carousel-3');
-      owl.owlCarousel({
-        autoplay: true,
-        lazyLoad: true,
-        rewind: true,
-        responsiveClass: true,
-        autoHeight: true,
-        autoplayTimeout: 7000,
-        smartSpeed: 800,
-        margin: 10,
-        loop: true,
-        responsive: {
-          0: {
-            items: 1
-          },
-          600: {
-            items: 2
-          },
-          1300: {
-            items: 3
-          },
-         
-          1600:{
-            items:3
-        }
-        }
-      })
+var owl = $("#owl-carousel-3");
+owl.owlCarousel({
+  autoplay: true,
+  lazyLoad: true,
+  rewind: true,
+  responsiveClass: true,
+  autoHeight: true,
+  autoplayTimeout: 7000,
+  smartSpeed: 800,
+  margin: 10,
+  loop: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1300: {
+      items: 3,
+    },
+
+    1600: {
+      items: 3,
+    },
+  },
+});
